9d763524a3bf3375bbe1cd965f42b52fcd69ecfc
FelixNameche <kevin.dubreucq@gmail.com>
2018-03-26 09:15:35 +0200

    dernier ajout

---
+++ html.php
@@ -0,0 +1,25 @@
+<?php
+    class Html
+    {
+        public function link($href, $rel)
+        {
+            return '<link href="'.$href.'" rel="'.$rel.'">';
+        }
+        public function stylesheet($href)
+        {
+            return $this->link($href, 'stylesheet');
+        }
+        public function image($src, $alt="")
+        {
+            return '<img src="'.$src.'" alt="'.$alt.'">';
+        }
+        public function a($href, $value)
+        {
+            return '<a href="'.$href.'" >'.$value.'</a>';
+        }
+        public function script($src)
+        {
+            return '<script src="'.$src.'"></script>';
+        }
+    }
+?>
